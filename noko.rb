659eb647109f6ccb890e3fb44b1b6461090e944b
Larry Charbonneau IV <larrycharbonneau4@gmail.com>
2019-03-16 16:15:03 -0400

    add nokogiri test script

---
+++ noko.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::XML(open('https://raw.githubusercontent.com/lawrence123/testfiles/master/test.xml'))
+
+latest = doc.xpath('//latest')
+
+puts latest.text
